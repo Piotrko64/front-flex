6931ae7fdc6db7074e49cf9141ddb7b43d047447
Piotrko64 <PiotrPc77@gmail.com>
2022-03-05 19:39:17 +0100

    jss is awesome!

--- src/templates/article.js
+++ src/templates/article.js
@@ -6,16 +6,7 @@ import Helmet from "react-helmet";
 import jss from "jss";
 import jssPluginGlobal from "jss-plugin-global";
 import { createUseStyles } from "react-jss";
-const styles = {
-    "@global": {
-        body: {
-            color: "green",
-        },
-        a: {
-            textDecoration: "underline",
-        },
-    },
-};
+
 const useStyles = createUseStyles({
     "@global": {
         body: {
@@ -25,25 +16,89 @@ const useStyles = createUseStyles({
             textDecoration: "underline",
         },
     },
+    article: {
+        backgroundColor: "#282c2f",
+        minHeight: "100vh",
+        fontFamily: "OSWALD",
+    },
+    article__back: {
+        backgroundPosition: "center",
+        backgroundRepeat: "noRepeat",
+        backgroundSize: "cover",
+
+        backgroundAttachment: "fixed",
+        height: "25vh",
+        maxWidth: "100vw",
+        position: "relative",
+
+        textTransform: "uppercase",
+        color: "black",
+        textAlign: "center",
+        display: "flex",
+        justifyContent: "center",
+    },
+    h1: {
+        margin: 0,
+        position: "absolute",
+        top: "100%",
+        fontWeight: 300,
+        transform: "translateY(-50%)",
+        padding: "0px 25px",
+        backgroundColor: "white",
+        fontSize: "3.2rem",
+        width: "min - content",
+
+        boxShadow: "0px 0px 24px 0px rgba(0, 0, 0, 1)",
+    },
+    article__describe: {
+        marginTop: "15vh",
+
+        padding: "5px",
+        maxWidth: "900px",
+        marginLeft: "auto",
+        marginRight: "auto",
+        backgroundColor: "rgb(41, 41, 41)",
+        color: "white",
+
+        boxShadow: "0px 0px 100px -1px rgba(41, 41, 41, 1)",
+    },
+    span: {
+        fontStyle: "italic",
+    },
+    p: {
+        fontWeight: 300,
+    },
+    article__gallery: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    img: {
+        maxWidth: "340px",
+        padding: "20px 0",
+    },
 });
 
 const Article = ({ pageContext: { slug }, data: { article } }) => {
-    useStyles();
+    const classes = useStyles();
     return (
         <>
             <Helmet>
                 <title>{article.title}</title>
             </Helmet>
 
-            <article className="article">
-                <div class="article__back" style={{ backgroundImage: `url(${article.background.url})` }}>
-                    <h1>{article.title}</h1>
+            <article className={classes.article}>
+                <div
+                    class={classes.article__back}
+                    style={{ backgroundImage: `url(${article.background.url})` }}
+                >
+                    <h1 class={classes.h1}>{article.title}</h1>
                 </div>
-                <div class="article__describe">
-                    <span>{article.data}</span>
+                <div class={classes.article__describe}>
+                    <span class={classes.span}>{article.data}</span>
                     <StructuredText data={article.content.value} />
-                    <div class="article__gallery">
-                        <img src={article.gallery[0].url} />
+                    <div class={classes.article__gallery}>
+                        <img src={article.gallery[0].url} class={classes.img} />
                     </div>
                 </div>
             </article>
