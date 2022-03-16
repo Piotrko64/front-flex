56779da66bf2cd6783e98116bc2b7ecbe7853ce0
Piotrko64 <PiotrPc77@gmail.com>
2022-03-16 21:59:23 +0100

    useEffect

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import FooterFront from "../components/FooterFront";
 import Layout from "../components/Layout";
 import MainFirst from "../components/homeComponents/MainFirst";
@@ -6,9 +6,20 @@ import MainSecond from "../components/homeComponents/MainSecond";
 import MainFourth from "../components/homeComponents/MainFourth";
 import MainThird from "../components/homeComponents/MainThird";
 function Main() {
-    const colors = ["#7982db", "#ffba26", "#972bd6", "#3a90c9", "#d62b46", "#972bd6", "#579346", "#20678d"];
-    const random = Math.round(Math.random() * colors.length);
-    document.documentElement.style.setProperty("--favourite", colors[random]);
+    useEffect(() => {
+        const colors = [
+            "#7982db",
+            "#ffba26",
+            "#972bd6",
+            "#3a90c9",
+            "#d62b46",
+            "#972bd6",
+            "#579346",
+            "#20678d",
+        ];
+        const random = Math.round(Math.random() * colors.length);
+        document.documentElement.style.setProperty("--favourite", colors[random]);
+    }, []);
     return (
         <>
             <Layout>
